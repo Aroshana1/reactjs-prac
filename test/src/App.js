108b4de3a9ce244a76733ae2ea2ca18579e666ae
Aroshana1 <aroshana6@gmail.com>
2024-01-01 23:02:39 +0530

    5 half

--- test/src/App.js
+++ test/src/App.js
@@ -4,11 +4,25 @@ import Main1 from './Components/Main1';
 //import myData from './Data/myData';
 import { myData } from './Data/myData';
 
+import {useState} from "react";
+
 
 
 
 
 function App() {
+
+  //let myVar = "Aroshana";
+  const [myVar, setMyVar] = useState("Aroshana")
+
+  const clickHandle = ()=>{
+  //myVar = "Sasanka";
+  setMyVar("Sasanka");
+  console.log(myVar);
+  }
+
+
+
   const mainBlock = myData.map(({name,city,age,id,image})=>{
     return <Main1 key = {id} name = {name} city = {city} age = {age} image = {image}/>;
   
@@ -17,12 +31,24 @@ function App() {
   return (
 
     <div className = "mainC">
-      <h5>Aroshana Dissanayake</h5>
+      <h5>{myVar}</h5>
   
   <div className = "mainContainer">
     {mainBlock}
 
   </div>
+  <br></br>
+  <button style = {
+    {
+    fontSize: "20px",
+    border:"1px solid red",
+    padding: "7px 12px",
+  }
+
+
+  } onClick = {clickHandle}>
+    click Me
+    </button>
   </div>
 
   );
